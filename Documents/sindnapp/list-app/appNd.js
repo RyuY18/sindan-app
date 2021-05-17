54cd784c2ad282f3868217ef44ffe49959ddb243
RyuY18 <nanimositakunai00@gmail.com>
2021-05-17 22:20:28 +0900

    nodeUp

--- Documents/sindnapp/list-app/appNd.js
+++ Documents/sindnapp/list-app/appNd.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
+app.use(express.static('js'));
 
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+  res.render('top.ejs');
 });
+
+app.get('/index',(req, res) => {
+  res.render('index.ejs')
+});
+
 
 // サーバーを起動するコードを貼り付けてください
 app.listen(3000);
