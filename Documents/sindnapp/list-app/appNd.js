18b596262593362305b8ce6d9fff9a742591e73d
RyuY18 <nanimositakunai00@gmail.com>
2021-05-18 23:34:30 +0900

    serbertest

--- Documents/sindnapp/list-app/appNd.js
+++ Documents/sindnapp/list-app/appNd.js
@@ -1,38 +1,9 @@
 const express = require("express");
 const app = express();
+const http = require('http');
 
 app.use(express.static("public"));
 app.use(express.static("js"));
-
-
-const passport = require('passport')
-, TwitterStrategy = require('passport-twitter').Strategy;
-
-passport.use(new TwitterStrategy({
-    // 上記アプリ登録で取得したキーを指定します。
-    consumerKey: TWITTER_CONSUMER_KEY,
-    consumerSecret: TWITTER_CONSUMER_SECRET,
-    // callbackで受け取りたいURLを指定します。
-    callbackURL: "http://www.example.com/auth/twitter/callback"
-  },
-  // Callback時の処理を記載します。
-  function(token, tokenSecret, profile, done) {
-    User.findOrCreate(..., function(err, user) {
-      if (err) { return done(err); }
-      // ここではユーザーが存在すれば、次の処理へ移ります。
-      done(null, user);
-    });
-  }
-));
-// 認証を開始する処理を記載します。
-app.get('/auth/twitter', passport.authenticate('twitter'));
-
-// コールバックで呼び出された際の処理を記載します。
-app.get('/auth/twitter/callback', 
-  passport.authenticate('twitter', { successRedirect: '/',
-                                     failureRedirect: '/login' }));
-
-
 
 app.get("/", (req, res) => {
   res.render("top.ejs");
